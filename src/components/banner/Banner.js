03f06045283f7afb52d7f265e929469d5dab4d6e
Tony Soukkeo <TonySoukkeo@Tonys-MacBook-Pro.local>
2019-05-29 20:51:16 -0600

    initital commit

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div id="banner">
+      <div id="overlay">
+        <div className="container">
+          <div id="banner-btn-outer">
+            <div id="banner-btn-inner">
+              <div id="banner-btn-view">View</div>
+            </div>
+          </div>
+
+          <div id="banner-text-box">
+            <p className="banner-name-text">Tony Soukkeo</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
