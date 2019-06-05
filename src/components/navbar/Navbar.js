41f2b36c7b3677e980b311503a27a676cf0cba2b
Tony Soukkeo <TonySoukkeo@Tonys-MBP.attlocal.net>
2019-06-04 22:00:53 -0700

    inititial commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -25,34 +25,6 @@ const Navbar = () => {
         </button>
 
         <div id="navbarNav" className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="tel:4068991762">
-                <i
-                  style={{
-                    color: "black",
-                    fontSize: "1.7rem"
-                  }}
-                  className="fas fa-phone"
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://github.com/TonySoukkeo"
-                target="_blank"
-              >
-                <i
-                  style={{
-                    color: "#007c00",
-                    fontSize: "2rem"
-                  }}
-                  className="fab fa-github-alt"
-                />
-              </a>
-            </li>
-          </ul>
           <div className="d-none d-md-block ml-auto">
             <ul className="navbar-nav ">
               <li className="nav-item">
@@ -74,17 +46,29 @@ const Navbar = () => {
           </div>
 
           <ul className="d-block d-md-none navbar-nav ml-auto">
-            <li className="nav-item">
+            <li
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              className="nav-item"
+            >
               <a href="#about" className="nav-link">
                 About
               </a>{" "}
             </li>
-            <li className="nav-item">
+            <li
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              className="nav-item"
+            >
               <a href="#projects" className="nav-link">
                 Projects
               </a>{" "}
             </li>
-            <li className="nav-item">
+            <li
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              className="nav-item"
+            >
               <a href="#contact" className="nav-link">
                 Contact
               </a>{" "}
